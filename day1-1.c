6f210795f07b3598270b118bc0fb9f2c215ee17a
yellowtides <andi.dicu12@gmail.com>
2019-12-13 15:00:30 +0200

    late start

---
+++ day1-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define ll long long
+
+int main() {
+	int val;
+	ll ans = 0;
+	do {
+		scanf("%d", &val);
+		if (val == -1) {
+			break;
+		}
+
+		// solution
+
+		ans += val/3-2;
+
+	} while (1);
+
+	printf("%lld", ans);
+
+	return 0;
+}
